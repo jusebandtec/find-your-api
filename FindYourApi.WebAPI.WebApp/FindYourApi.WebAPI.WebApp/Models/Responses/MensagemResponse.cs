7973acc5f03ade2878207e70947d74da5a52abc7
jusebandtec <jose.silva@bandtec.com.br>
2021-03-28 21:40:19 -0300

    aplicação Find Your Api

---
+++ FindYourApi.WebAPI.WebApp/FindYourApi.WebAPI.WebApp/Models/Responses/MensagemResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindYourApi.WebAPI.WebApp.Models.Responses
+{
+    public class MensagemResponse
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+    }
+}
